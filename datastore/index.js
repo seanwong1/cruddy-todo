357e49e0d3177533544deb01839749515732b528
BradySBaker <bradysamuelbaker@gmail.com>
2023-01-14 11:59:53 -0800

    Complete Bare Minimum Requirements

--- datastore/index.js
+++ datastore/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
+var Promise = require('bluebird');
+Promise.promisifyAll(fs);
 
 var items = {};
 
@@ -25,6 +27,20 @@ exports.create = (text, callback) => {
   });
 };
 
+var getText = function(fileName) {
+  return fs.readFileAsync(path.join(exports.dataDir, fileName));
+};
+
+var getAllText = function(callback) {
+  var promises = [];
+  fs.readdirAsync(exports.dataDir).then((files) => {
+    for (var i = 0; i < files.length; i++) {
+      promises.push(getText(files[i]));
+    }
+    callback(Promise.all(promises));
+  });
+};
+
 exports.readAll = (callback) => {
 //Use the file where id's are stored to loop through each id
 //[{id: 001.txt, text: 001.txt}];
@@ -34,12 +50,35 @@ exports.readAll = (callback) => {
       callback(err, null);
     } else {
       var data = _.map(files, (file) => {
-        return { id: file.slice(0, file.length - 4), text: file.slice(0, file.length - 4) };
+        return {id: file.slice(0, file.length - 4), text: undefined};
       });
-      callback(null, data);
+      getAllText((promises) => promises.then((textArr) => {
+        for (var i = 0; i < textArr.length; i++) {
+          data[i].text = textArr[i].toString();
+        }
+        callback(null, data);
+      }));
+      //callback(null, Promise.all(data.text));
     }
   });
 };
+
+
+//  function getAllImages() {
+//     var promises = [];
+//     // load all images in parallel
+//     for (var i = 0; i <= 2; i++) {
+//         promises.push(getImage(i));
+//     }
+//     // return promise that is resolved when all images are done loading
+//     return Promise.all(promises);
+//  }
+
+//  getAllImages().then(function(imageArray) {
+//     // you have an array of image data in imageArray
+//  }, function(err) {
+//     // an error occurred
+//  });
 
 exports.readOne = (id, callback) => {
   fs.readFile(path.join(exports.dataDir, id + '.txt'), (err, text) => {
